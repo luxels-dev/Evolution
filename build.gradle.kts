a569d40425faf76b604b2c2f9c1991531712993b
Luxels <luxels.spam@gmail.com>
2026-06-04 18:09:52 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("jvm") version libs.versions.kotlin.get()
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.run.paper)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+    implementation(libs.kotlin.stdlib)
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    runServer {
+        minecraftVersion(libs.versions.minecraft.get())
+        jvmArgs("-Xms2G", "-Xmx2G", "-Dcom.mojang.eula.agree=true")
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/kotlin/com/helydra/evolutionPlugin/EvolutionPlugin.kt
@@ -0,0 +1,14 @@
+package com.helydra.evolutionPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EvolutionPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
